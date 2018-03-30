f67e177fa464a57e8ddf08bcfa3a6c35ca9e7346
Alex Korostylev <ac.korostylev@gmail.com>
2018-03-30 18:13:18 +0300

    intent from page "add farm" to "about us"

--- app/src/main/java/spain/barcelona/rentrender/AddFarm.java
+++ app/src/main/java/spain/barcelona/rentrender/AddFarm.java
@@ -2,6 +2,8 @@ package spain.barcelona.rentrender;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class AddFarm extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AddFarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_farm);
+    }
+    //Вызвать onSendMessage() при щелчке на кнопке
+    public void onSendMessage(View view) {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
     }
 }
