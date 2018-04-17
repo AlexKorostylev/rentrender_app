d4b7c417af15defe7dee9de59301afb6d7ab323b
Alex Korostylev <ac.korostylev@gmail.com>
2018-04-17 13:49:59 +0300

    Drawer. List of item at the panel
    (cherry picked from commit 9814ca8)

--- app/src/main/java/spain/barcelona/rentrender/MainList.java
+++ app/src/main/java/spain/barcelona/rentrender/MainList.java
@@ -1,40 +1,26 @@
 package spain.barcelona.rentrender;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.support.v4.widget.DrawerLayout;
 
-public class MainList extends AppCompatActivity {
+public class MainList extends Activity {
+
+    private String[] titles;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView)findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_activated_1, titles));
     }
-    // При щелчке на кнопке
-    public void onAboutUs(View view) {
-        Intent intent = new Intent(this, About.class);
-        startActivity(intent);
-    }
-    public void onAddFarm(View view) {
-        Intent intent = new Intent(this, AddFarm.class);
-        startActivity(intent);
-    }
-    public void onEditFarm(View view) {
-        Intent intent = new Intent(this, EditFarm.class);
-        startActivity(intent);
-    }
-    public void onContactUs(View view) {
-        Intent intent = new Intent(this, Contact.class);
-        startActivity(intent);
-    }
-    public void onAdvert(View view) {
-        Intent intent = new Intent(this, Advert.class);
-        startActivity(intent);
-    }
-    public void onFarmDetail(View view) {
-        Intent intent = new Intent(this, FarmDetail.class);
-        startActivity(intent);
-    }
+
 }
