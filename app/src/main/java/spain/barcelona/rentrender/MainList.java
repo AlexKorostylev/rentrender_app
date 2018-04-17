5a580b83b68a0e135484b65f7d503ff802a79eaa
Alex Korostylev <ac.korostylev@gmail.com>
2018-04-17 14:36:23 +0300

    Drawer. Some comments for code
    (cherry picked from commit ce0bd77)

--- app/src/main/java/spain/barcelona/rentrender/MainList.java
+++ app/src/main/java/spain/barcelona/rentrender/MainList.java
@@ -1,26 +1,27 @@
 package spain.barcelona.rentrender;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.support.v4.widget.DrawerLayout;
-import android.view.View;
 import android.widget.AdapterView;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 
 public class MainList extends Activity {
 
+
     private String[] titles;
     private ListView drawerList;
+    private DrawerLayout drawerLayout;
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//Код, выполняемый при щелчке на элементе списка.
+            //Код, выполняемый при щелчке на элементе списка.
+            //Code to run when an item in the navigation drawer gets clicked
             selectItem(position);
         }
     }
@@ -31,15 +32,23 @@ public class MainList extends Activity {
         setContentView(R.layout.activity_main_list);
         titles = getResources().getStringArray(R.array.titles);
         drawerList = (ListView) findViewById(R.id.drawer);
+        // получение ссылки на DrawerLayout
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerList.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_activated_1, titles));
         drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        // При исходном создании MainActivity использовать метод selectItem()
+        // для отображения TopFragment
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
     }
 
     private void selectItem(int position) {
         Fragment fragment;
         switch(position) {
             case 1:
+                // Определить, какой фрагмент должен отображаться
                 fragment = new SoftFragment();
                 break;
             case 2:
@@ -51,12 +60,12 @@ public class MainList extends Activity {
             default:
                 fragment = new TopFragment();
         }
+        // Вывести фрагмент с использованием транзакции фрагмента
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         ft.replace(R.id.content_frame, fragment);
         ft.addToBackStack(null);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         ft.commit();
-
         //Назначение заголовка панели действий (action bar).
         setActionBarTitle(position);
         //Закрыть выдвижную панель
@@ -66,10 +75,13 @@ public class MainList extends Activity {
     private void setActionBarTitle(int position) {
         String title;
         if (position == 0){
+            // Если пользователь выбирает вариант “Home”, в качестве
+            //текста заголовка используется имя приложения.
             title = getResources().getString(R.string.app_name);
         } else {
             title = titles[position];
         }
+        // Вывести заголовок на панели действий
         getActionBar().setTitle(title);
     }
 
