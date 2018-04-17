5738c409251ccc720dd13f703c9920e4b6eebc67
Alex Korostylev <ac.korostylev@gmail.com>
2018-04-17 14:11:37 +0300

    Drawer. OnItemClickListener, selectItem
    (cherry picked from commit 45087c6)

--- app/src/main/java/spain/barcelona/rentrender/MainList.java
+++ app/src/main/java/spain/barcelona/rentrender/MainList.java
@@ -1,26 +1,61 @@
 package spain.barcelona.rentrender;
 
-import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 
 public class MainList extends Activity {
 
     private String[] titles;
     private ListView drawerList;
 
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//Код, выполняемый при щелчке на элементе списка.
+            selectItem(position);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
         titles = getResources().getStringArray(R.array.titles);
-        drawerList = (ListView)findViewById(R.id.drawer);
+        drawerList = (ListView) findViewById(R.id.drawer);
         drawerList.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_activated_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment;
+        switch(position) {
+            case 1:
+                fragment = new SoftFragment();
+                break;
+            case 2:
+                fragment = new CountryFragment();
+                break;
+            case 3:
+                fragment = new PriceFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
     }
 
 }
