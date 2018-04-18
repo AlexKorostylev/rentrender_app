a90a06ccbe691b8e046aa828ad94455885663b1b
Alex Korostylev <ac.korostylev@gmail.com>
2018-04-18 20:39:51 +0300

    intent FarmDetailActivity from CardFarm
    (cherry picked from commit a5d33a5)

--- app/src/main/java/spain/barcelona/rentrender/FarmDetailActivity.java
+++ app/src/main/java/spain/barcelona/rentrender/FarmDetailActivity.java
@@ -1,13 +1,26 @@
 package spain.barcelona.rentrender;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class FarmDetailActivity extends AppCompatActivity {
+public class FarmDetailActivity extends Activity {
+
+    public static final String EXTRA_FARMNO = "farmNo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_detail);
+
+        //Включение кнопки Вверх
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Вывод подробной информации о пицце
+        int farmNo = (Integer)getIntent().getExtras().get(EXTRA_FARMNO);
+        String farmName = CardFarm.farms[farmNo].getName();
+        TextView textView = (TextView)findViewById(R.id.farm_text_name);
+        textView.setText(farmName);
     }
 }
