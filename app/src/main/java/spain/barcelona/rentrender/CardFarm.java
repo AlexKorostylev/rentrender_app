fa753c721ade97626366dc32d0b7cdc0fb1638c2
Alex Korostylev <ac.korostylev@gmail.com>
2018-04-27 11:54:37 +0300

    Ready RecycleView farm's card

--- app/src/main/java/spain/barcelona/rentrender/CardFarm.java
+++ app/src/main/java/spain/barcelona/rentrender/CardFarm.java
@@ -1,21 +1,32 @@
 package spain.barcelona.rentrender;
 
 public class CardFarm {
-    private String name;
     private int imageResourceId;
+    private String name;
+    private String power;
+    private String price;
 
     public static final CardFarm[] farms = {
-            new CardFarm("RebusFarm", R.drawable.classic_render_farm),
-            new CardFarm("Sheepit! Render Farm", R.drawable.cloud_render_farm)
+            new CardFarm(R.drawable.classic_farm,"RebusFarm", "0.003 per Ghz.H","5 THz / 5,000 GHz" ),
+            new CardFarm(R.drawable.cloud_farm,"Copernicus Computing", "$0.004/Ghz /hour","22 500 Ghz + 15 superfast GPU Supermicro server with 10 Ge-Force 10-80 Ti" ),
+            new CardFarm(R.drawable.community_farm,"PeaRender", "<1$ per node per hour","more then 1000 GHz" )
     };
-    private CardFarm(String name, int imageResourceId) {
-        this.name = name;
+    private CardFarm(int imageResourceId, String name, String price, String power) {
         this.imageResourceId = imageResourceId;
+        this.name = name;
+        this.power = power;
+        this.price = price;
+    }
+    public int getImageResourceId() {
+        return imageResourceId;
     }
     public String getName() {
         return name;
     }
-    public int getImageResourceId() {
-        return imageResourceId;
+    public String getPower() {
+        return power;
+    }
+    public String getPrice() {
+        return price;
     }
 }
